9fd7a2ab29f2a5f7a2ff6fbef7ff24950a8f6c02
V. Shivaramakrishnan <vsrkrishnan.1987@gmail.com>
2022-06-01 22:23:32 +0530

    Fixing a couple of bugs, especially in the creation of the launch_attack.sh script. The curl command was missing the correct headers to carry out the attack.

--- terraform/modules/vpc/vul-app-svr.sh
+++ terraform/modules/vpc/vul-app-svr.sh
@@ -15,3 +15,8 @@ sudo docker info
 
 # Downloading and Running the Vulnerable App Server Demo App
 sudo docker container run -itd --rm --name vul-app-1 -p 8080:8080 us.gcr.io/panw-gcp-team-testing/qwiklab/pcc-log4shell/l4s-demo-app:1.0
+
+# Updating the /etc/hosts file to add a DNS entry for the attack server
+# Commenting this for now as the entry is not added as intended at the time of server bringup. Need to
+# revisit this place later.
+# sudo docker exec -it vul-app-1 /bin/sh -c 'echo "10.2.1.100    att-svr" >> /etc/hosts'
